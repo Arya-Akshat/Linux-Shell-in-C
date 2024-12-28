d24e61b3d88c6ed5594eb3d0eb13f34d6abaaf25
Arya-Akshat <154618237+Arya-Akshat@users.noreply.github.com>
2024-12-28 17:18:49 +0530

    Update shell.c

--- shell.c
+++ shell.c
@@ -95,7 +95,7 @@ int main(int argc, char* argv[]) {
         while(1) {
 
                 bzero(line, sizeof(line));
-                printf("<<ishanarya0>> ");
+                printf("<<Arya>> ");
 
                 scanf("%[^\n]", line);
                 getchar();
